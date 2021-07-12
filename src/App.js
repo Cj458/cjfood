0c33efbd35d0e0f58e10db8f1943f99b4f0bc42a
Cj458 <Calebjason90@gmail.com>
2021-07-12 03:45:23 +0300

    heroku deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {CssBaseline, 
+  ThemeProvider, 
+  Container, 
+  Paper, 
+  createMuiTheme} from "@material-ui/core";
+import {Route, BrowserRouter } from "react-router-dom";
+import ChooseScreen from './screens/ChooseScreen';
+import HomeScreen from "./screens/HomeScreen";
+import OrderScreen from "./screens/OrderScreen";
+import ReviewScreen from "./screens/ReviewScreen";
+import SelectPaymentScreen from "./screens/SelectPaymentScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import CompleteOrderScreen from "./screens/CompleteOrderScreen";
 
-function App() {
+
+
+  const theme = createMuiTheme({
+   typography: {
+     h1: { fontWeight: 'bold'},
+     h2: {
+       fontSize: '2rem',
+       color: 'black',
+     },
+
+     h3: {
+      fontSize: '1.8rem',
+      fontWeight: 'bold',
+      color: 'white',
+    },
+   },
+   palette: {
+     primary: { main: "#ff1744"},
+     secondary: {
+      main: "#118e16",
+      contrastText: '#ffffff',
+     },
+   },
+  });
+  function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+         <Container maxWidth="sm">
+          <Paper>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/choose" component={ChooseScreen} exact></Route>
+          <Route path="/order" component={OrderScreen} exact></Route>
+          <Route path="/review" component={ReviewScreen} exact></Route>
+          <Route path="/select-payment" component={SelectPaymentScreen} exact></Route>
+          <Route path="/payment" component={PaymentScreen} exact></Route>
+          <Route path="/complete" component={CompleteOrderScreen} exact></Route>
+          </Paper>
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
